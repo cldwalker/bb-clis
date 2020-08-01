c7c3d3b9ae9359bda3777030edeb7ab919a9dc87
Gabriel Horner <gabriel.horner@gmail.com>
2020-08-01 02:09:14 -0400

    Add a helpful exploration fn

---
+++ src/cldwalker/babashka/util/maven_artifact.clj
@@ -0,0 +1,23 @@
+(ns cldwalker.babashka.util.maven-artifact
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.java.shell :as shell]))
+
+(defn artifact-files
+  "List clj/cljs/cljc files in a maven artifact. When orienting yourself
+in a new library, this fn and (dir) are helpful for quick api exploration."
+  [artifact version]
+  (let [artifact_ (if (.contains artifact "/")
+                    artifact
+                    (str artifact "/" artifact))]
+    (if-let [jar-file (->> (format "%s/.m2/repository/%s/%s"
+                                   (System/getenv "HOME") artifact_ version)
+                           io/file
+                           .listFiles
+                           seq
+                           (filter #(re-find #"\.jar$" (str %)))
+                           first)]
+      (->> (str/split-lines (:out (shell/sh "jar" "-tf" (str jar-file))))
+           (filter #(re-find #"\.(clj|cljs|cljc)$" %))
+           (remove #(re-find #"project.clj$" %)))
+      (println "No jar file found"))))
