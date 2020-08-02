e2c6b9985c5748d85a2c2282afaf233eaf800110
Gabriel Horner <gabriel.horner@gmail.com>
2020-08-02 16:57:34 -0400

    Add new bb-table tests thx to bb-cli-test

--- test/bin/bb_table_test.clj
+++ test/bin/bb_table_test.clj
@@ -12,3 +12,21 @@
     (is (= (:exit expected-results) (:exit cmd-results)))
     (is (= (:out expected-results) (:out cmd-results)))
     (is (= (:err expected-results) (:err cmd-results)))))
+
+(deftest map-stdin-argument
+  (let [cmd-results (shell/sh "bash" "-c" "echo {:a 1 :b 2} | bb-table")
+        expected-results (-> (io/resource "bin/bb_table_test/map-stdin-argument.edn")
+                             slurp
+                             edn/read-string)]
+    (is (= (:exit expected-results) (:exit cmd-results)))
+    (is (= (:out expected-results) (:out cmd-results)))
+    (is (= (:err expected-results) (:err cmd-results)))))
+
+(deftest vec-stdin-argument
+  (let [cmd-results (shell/sh "bash" "-c" "echo '[[:a :b] [:d :e]]' | bb-table")
+        expected-results (-> (io/resource "bin/bb_table_test/vec-stdin-argument.edn")
+                             slurp
+                             edn/read-string)]
+    (is (= (:exit expected-results) (:exit cmd-results)))
+    (is (= (:out expected-results) (:out cmd-results)))
+    (is (= (:err expected-results) (:err cmd-results)))))
