453433e4a6d082d78751eb175b08bf3351481d9f
Gabriel Horner <gabriel.horner@gmail.com>
2020-08-05 02:35:44 -0400

    Clean setup and teardown of replacement file

--- test/bin/bb_replace_test.clj
+++ test/bin/bb_replace_test.clj
@@ -16,7 +16,8 @@
     (is (= (:err expected-results) (:err cmd-results)))))
 
 (deftest standard-replacement
-  (spit (io/resource "bin/bb_replace_test/project.clj") "(defproject bar \"1.0.0\")")
+  (spit "test/resources/bin/bb_replace_test/project.clj" "(defproject bar \"1.0.0\")")
+
   (let [cmd-results (shell/sh "bb-replace" "lein-version" "2.0.0"
                               :dir "test/resources/bin/bb_replace_test")
         expected-results (-> (io/resource "bin/bb_replace_test/standard-replacement.edn")
@@ -25,4 +26,6 @@
     (is (= (:exit expected-results) (:exit cmd-results)))
     (is (= (:out expected-results) (:out cmd-results)))
     (is (= (:err expected-results) (:err cmd-results)))
-    (is (str/includes? (slurp (io/resource "bin/bb_replace_test/project.clj")) "2.0.0"))))
+    (is (str/includes? (slurp (io/resource "bin/bb_replace_test/project.clj")) "2.0.0"))
+
+    (io/delete-file "test/resources/bin/bb_replace_test/project.clj")))
