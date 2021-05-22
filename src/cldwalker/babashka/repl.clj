44a1732a35a7ff898cfe780a23e98f66a34610cb
Gabriel Horner <gabriel.horner@gmail.com>
2021-05-21 22:22:55 -0400

    Add portal repl helper

---
+++ src/cldwalker/babashka/repl.clj
@@ -0,0 +1,11 @@
+(ns cldwalker.babashka.repl
+  "Fns that are useful in a bb repl"
+  (:require [babashka.deps :as deps]))
+
+(defn add-portal
+  []
+  (deps/add-deps '{:deps {djblue/portal {:mvn/version "RELEASE"}}})
+  (require '[portal.api :as p])
+  (let [p ((requiring-resolve 'portal.api/open))]
+    (add-tap (requiring-resolve 'portal.api/submit))
+    p))
