66319cc2a0114aeac779c680e134e9fec139bd09
Gabriel Horner <gabriel.horner@gmail.com>
2021-08-15 00:46:08 -0400

    Move logseq helpers to new ns

---
+++ src/cldwalker/bb_clis/cli/logseq.clj
@@ -0,0 +1,16 @@
+(ns cldwalker.bb-clis.cli.logseq
+  "Cli fns for logseq"
+  (:require [clojure.string :as str]))
+
+(defn block->properties [block]
+  (->> block
+       str/split-lines
+       (map #(str/split % #"::\s*"))
+       ;; TODO: warn when removing lines
+       (remove #(< (count %) 2))
+       (into {})))
+
+(defn properties->block [properties]
+  (->> properties
+       (map (fn [[k v]] (str k ":: " v)))
+       (str/join "\n")))
