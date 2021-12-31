7f38c65d021d5644615f080a1b0d25e8fc5bfadf
Gabriel Horner <gabriel.horner@gmail.com>
2021-12-31 00:33:56 -0500

    Update docs and bin to reflect much simpler usage with 0.7.2
    Also organized docs and removed unnecessary map-keys

--- preloads.clj
+++ preloads.clj
@@ -1,5 +1,2 @@
 ;; This file is loaded by $BABASHKA_PRELOADS
-(require '[cldwalker.bb-clis.util.associative :refer [map-keys
-                                                       map-vals]])
-
 (require '[clojure.pprint :refer [pprint]])
