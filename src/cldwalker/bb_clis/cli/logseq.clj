86b9b3c1a18ddece55f8f2d9d667ba1049a6a561
Gabriel Horner <gabriel.horner@gmail.com>
2026-06-08 02:00:42 -0400

    rm unused clis

--- src/cldwalker/bb_clis/cli/logseq.clj
+++ src/cldwalker/bb_clis/cli/logseq.clj
@@ -2,14 +2,6 @@
   "Cli fns for logseq"
   (:require [clojure.string :as str]))
 
-(defn block->properties [block]
-  (->> block
-       str/split-lines
-       (map #(str/split % #"::\s*"))
-       ;; TODO: warn when removing lines
-       (remove #(< (count %) 2))
-       (into {})))
-
 (defn properties->block [properties]
   (->> properties
        (map (fn [[k v]] (str (name k) ":: " v)))
