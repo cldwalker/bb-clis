e20ff4bc40a9c45d12f331b10bd7b50a8b9a1f26
Gabriel Horner <gabriel.horner@gmail.com>
2026-08-03 23:04:01 -0400

    Add -o/--object-urls to monitor + improve objects w/o urls

--- src/cldwalker/bb_clis/bin/logseq_graph_stats.clj
+++ src/cldwalker/bb_clis/bin/logseq_graph_stats.clj
@@ -51,8 +51,8 @@
       (cli-util/error "search property failed:" out))
     (some #(when (= "url" (:block/title %)) (:db/ident %)) (:items data))))
 
-(defn- tag->url [graph]
-  (if-let [ident (url-property-ident graph)]
+(defn- tag->url [graph ident]
+  (if ident
     (->> (logseq-query graph
                        [:find '?title '?url
                         :where
@@ -63,6 +63,18 @@
          (into {}))
     {}))
 
+(defn- objects-with-url-pairs
+  "Per class, count of objects that have their own url property value."
+  [graph ident]
+  (if ident
+    (logseq-query graph
+                  [:find '?title '(count ?e)
+                   :where
+                   ['?e :block/tags '?t]
+                   ['?t :block/title '?title]
+                   ['?e ident]])
+    []))
+
 (defn- graph-counts [graph user?]
   (let [extra (when user? ["--include-built-in" "false"])]
     (array-map
@@ -71,6 +83,9 @@
      :classes (apply logseq-list-count graph "tag" extra)
      :properties (apply logseq-list-count graph "property" extra))))
 
+(defn- truncate [s length]
+  (if (> (count s) length) (str (subs s 0 length) "...") s))
+
 (defn- pct [num denom]
   (if (zero? denom) "n/a" (format "%.1f%%" (* 100.0 (/ num (double denom))))))
 
@@ -78,34 +93,82 @@
   (let [pairs (logseq-query graph '[:find ?title (count ?e)
                                     :where [?e :block/tags ?t] [?t :block/title ?title]])
         builtins (when user (builtin-class-titles graph))
-        pairs' (cond->> pairs
-                 user (remove #(builtins (first %))))
-        urls (tag->url graph)
+        remove-builtins (fn [pairs]
+                          (cond->> pairs
+                            user (remove #(builtins (first %)))))
+        pairs' (remove-builtins pairs)
+        ident (url-property-ident graph)
+        urls (tag->url graph ident)
         rows (->> pairs'
                   (sort-by second >)
-                  (map (fn [[cls c]] {"Class" cls "Url" (get urls cls "") "Count" c})))
+                  (map (fn [[cls c]] {"Class" cls "Url" (truncate (get urls cls "") 80) "Count" c})))
         total-objects (apply + (map second pairs'))
         total-classes (count pairs')
         objects-with-url (->> pairs' (filter #(get urls (first %))) (map second) (apply +))
-        classes-with-url (->> pairs' (filter #(get urls (first %))) count)]
+        classes-with-url (->> pairs' (filter #(get urls (first %))) count)
+        objects-with-own-url (->> (objects-with-url-pairs graph ident)
+                                  remove-builtins
+                                  (map second)
+                                  (apply +))]
     (pprint/print-table ["Class" "Url" "Count"] rows)
     (println "Total objects:" total-objects)
     (println "Total classes:" total-classes)
-    (println "Objects with url:"
-             (format "%d/%d = %s"
-                     objects-with-url total-objects (pct objects-with-url total-objects)))
     (println "Classes with url:"
              (format "%d/%d = %s"
-                     classes-with-url total-classes (pct classes-with-url total-classes)))))
+                     classes-with-url total-classes (pct classes-with-url total-classes)))
+    (println "Objects with class url:"
+             (format "%d/%d = %s"
+                     objects-with-url total-objects (pct objects-with-url total-objects)))
+    (println "Objects with url:"
+             (format "%d/%d = %s"
+                     objects-with-own-url total-objects (pct objects-with-own-url total-objects)))))
+
+(defn- object-urls-table
+  "Print all objects with their own url, no urls first."
+  [graph {:keys [user]}]
+  (let [ident (url-property-ident graph)
+        objects (logseq-query graph '[:find ?e ?title ?class
+                                      :where
+                                      [?e :block/tags ?t]
+                                      [?e :block/title ?title]
+                                      [?t :block/title ?class]])
+        builtins (when user (builtin-class-titles graph))
+        objects' (cond->> objects
+                   user (remove #(builtins (nth % 2))))
+        e->url (if ident
+                 (->> (logseq-query graph
+                                    [:find '?e '?url
+                                     :where
+                                     ['?e :block/tags]
+                                     ['?e ident '?url-ref]
+                                     ['?url-ref :block/title '?url]])
+                      (into {}))
+                 {})
+        rows (->> objects'
+                  (map (fn [[e title _class]] [e title]))
+                  distinct
+                  (map (fn [[e title]] {"Title" (truncate title 50) "Url" (truncate (get e->url e) 50)}))
+                  (sort-by (juxt #(if (nil? (get % "Url")) 0 1)
+                                 #(get % "Title"))))]
+    (pprint/print-table ["Title" "Url"] rows)
+    (println "Objects with url:"
+             (format "%d/%d = %s"
+                     (count (filter #(get % "Url") rows)) (count rows)
+                     (pct (count (filter #(get % "Url") rows)) (count rows))))))
 
 (defn- command [{:keys [opts]}]
-  (println "Object Counts by Class:")
-  (object-counts (:graph opts) opts)
-  (println "\nCounts:")
-  (pprint/pprint (graph-counts (:graph opts) (:user opts))))
+  (if (:object-urls opts)
+    (object-urls-table (:graph opts) opts)
+    (do
+      (println "Object Counts by Class:")
+      (object-counts (:graph opts) opts)
+      (println "\nCounts:")
+      (pprint/pprint (graph-counts (:graph opts) (:user opts))))))
 
 (def ^:private spec
   {:graph {:alias :g :desc "Graph name"}
+   :object-urls {:alias :o :coerce :boolean
+                 :desc "Display all objects and their urls, no urls first"}
    :user {:alias :u :coerce :boolean :desc "Exclude built-in pages, classes and properties"}})
 
 (defn -main [& args]
